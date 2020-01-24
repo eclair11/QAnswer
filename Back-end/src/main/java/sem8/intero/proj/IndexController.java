e5d39934a234fbfb19ea90eba788fe05cb6f49aa
Elias Romdan <elias.romdan@etu.univ-st-etienne>
2020-01-24 11:00:19 +0100

    Réorganisation du projet

--- Back-end/src/main/java/sem8/intero/proj/IndexController.java
+++ Back-end/src/main/java/sem8/intero/proj/IndexController.java
@@ -3,7 +3,6 @@ package sem8.intero.proj;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import sem8.intero.proj.Repository.DemandeRepository;
