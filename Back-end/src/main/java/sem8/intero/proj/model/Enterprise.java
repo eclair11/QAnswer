90df32ce57b0700c003c06f9e4ca8740e8b743ca
Elias Romdan <elias.romdan@etu.univ-st-etienne.fr>
2020-02-28 13:26:49 +0100

    Ajout du document d'avancement de projet + Mise à jour du parseur CSV

--- Back-end/src/main/java/sem8/intero/proj/model/Enterprise.java
+++ Back-end/src/main/java/sem8/intero/proj/model/Enterprise.java
@@ -16,67 +16,46 @@ public class Enterprise {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
-/*
-    @CsvBindByName(column = "SIREN")
+
+    @CsvBindByName
     String siren;
 
-    @CsvBindByName(column = "NIC")
+    @CsvBindByName
     String nic;
 
     @CsvBindByName
     String siret;
 
-    @CsvBindByName(column = "Date de création de l'établissement")
-    String creation;
+    @CsvBindByName
+    String dateCreationEtablissement;
 
-    @CsvBindByName(column = "Tranche de l'effectif de l'établissement")
-    String effectif;
+    @CsvBindByName
+    String numeroVoieEtablissement;
 
-    @CsvBindByName(column = "Catégorie de l'entreprise")
-    String categorie;
+    @CsvBindByName
+    String indiceRepetitionEtablissement;
 
-    @CsvBindByName(column = "Numéro de voie de l'établissement")
-    String num_voie;
+    @CsvBindByName
+    String typeVoieEtablissement;
 
-    @CsvBindByName(column = "Indice de répétition de l'établissement")
-    String ind_repet;
+    @CsvBindByName
+    String libelleVoieEtablissement;
 
-    @CsvBindByName(column = "Type de voie de l'établissement")
-    String type_voie;
+    @CsvBindByName
+    String codePostalEtablissement;
 
-    @CsvBindByName(column = "Libellé de la voie de l'établissement")
-    String nom_voie;
+    @CsvBindByName
+    String enseigne1Etablissement;
 
-    @CsvBindByName(column = "Code postal de l'établissement")
-    String code_postal;
+    @CsvBindByName
+    String longitude;
 
-    @CsvBindByName(column = "Enseigne de l'établissement 1")
-    String enseigne;
+    @CsvBindByName
+    String latitude;
 
-    @CsvBindByName(column = "Section de l'établissement")
-    String section;
-
-    @CsvBindByName(column = "Groupe de l'établissement")
-    String groupe;
-
-    @CsvBindByName(column = "Civilité de la personne physique")
-    String type_personne;
-
-    @CsvBindByName(column = "Prénom de la personne physique 1")
-    String prenom_personne;
-
-    @CsvBindByName(column = "Nom de la personne physique")
-    String nom_personne;
-
-    @CsvBindByName(column = "Nature juridique de l'unité légale")
-    String nature_juridique;
-
-    @CsvBindByName(column = "Géolocalisation de l'établissement")
-    String localisation;
-*/
     public Enterprise() {
     }
-/*
+
     public Long getId() {
         return id;
     }
@@ -109,132 +88,76 @@ public class Enterprise {
         this.siret = siret;
     }
 
-    public String getCreation() {
-        return creation;
+    public String getDateCreationEtablissement() {
+        return dateCreationEtablissement;
     }
 
-    public void setCreation(String creation) {
-        this.creation = creation;
+    public void setDateCreationEtablissement(String dateCreationEtablissement) {
+        this.dateCreationEtablissement = dateCreationEtablissement;
     }
 
-    public String getEffectif() {
-        return effectif;
+    public String getNumeroVoieEtablissement() {
+        return numeroVoieEtablissement;
     }
 
-    public void setEffectif(String effectif) {
-        this.effectif = effectif;
+    public void setNumeroVoieEtablissement(String numeroVoieEtablissement) {
+        this.numeroVoieEtablissement = numeroVoieEtablissement;
     }
 
-    public String getCategorie() {
-        return categorie;
+    public String getIndiceRepetitionEtablissement() {
+        return indiceRepetitionEtablissement;
     }
 
-    public void setCategorie(String categorie) {
-        this.categorie = categorie;
+    public void setIndiceRepetitionEtablissement(String indiceRepetitionEtablissement) {
+        this.indiceRepetitionEtablissement = indiceRepetitionEtablissement;
     }
 
-    public String getNum_voie() {
-        return num_voie;
+    public String getTypeVoieEtablissement() {
+        return typeVoieEtablissement;
     }
 
-    public void setNum_voie(String num_voie) {
-        this.num_voie = num_voie;
+    public void setTypeVoieEtablissement(String typeVoieEtablissement) {
+        this.typeVoieEtablissement = typeVoieEtablissement;
     }
 
-    public String getInd_repet() {
-        return ind_repet;
+    public String getLibelleVoieEtablissement() {
+        return libelleVoieEtablissement;
     }
 
-    public void setInd_repet(String ind_repet) {
-        this.ind_repet = ind_repet;
+    public void setLibelleVoieEtablissement(String libelleVoieEtablissement) {
+        this.libelleVoieEtablissement = libelleVoieEtablissement;
     }
 
-    public String getType_voie() {
-        return type_voie;
+    public String getCodePostalEtablissement() {
+        return codePostalEtablissement;
     }
 
-    public void setType_voie(String type_voie) {
-        this.type_voie = type_voie;
+    public void setCodePostalEtablissement(String codePostalEtablissement) {
+        this.codePostalEtablissement = codePostalEtablissement;
     }
 
-    public String getNom_voie() {
-        return nom_voie;
+    public String getEnseigne1Etablissement() {
+        return enseigne1Etablissement;
     }
 
-    public void setNom_voie(String nom_voie) {
-        this.nom_voie = nom_voie;
+    public void setEnseigne1Etablissement(String enseigne1Etablissement) {
+        this.enseigne1Etablissement = enseigne1Etablissement;
     }
 
-    public String getCode_postal() {
-        return code_postal;
+    public String getLongitude() {
+        return longitude;
     }
 
-    public void setCode_postal(String code_postal) {
-        this.code_postal = code_postal;
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
     }
 
-    public String getEnseigne() {
-        return enseigne;
+    public String getLatitude() {
+        return latitude;
     }
 
-    public void setEnseigne(String enseigne) {
-        this.enseigne = enseigne;
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
     }
 
-    public String getSection() {
-        return section;
-    }
-
-    public void setSection(String section) {
-        this.section = section;
-    }
-
-    public String getGroupe() {
-        return groupe;
-    }
-
-    public void setGroupe(String groupe) {
-        this.groupe = groupe;
-    }
-
-    public String getType_personne() {
-        return type_personne;
-    }
-
-    public void setType_personne(String type_personne) {
-        this.type_personne = type_personne;
-    }
-
-    public String getPrenom_personne() {
-        return prenom_personne;
-    }
-
-    public void setPrenom_personne(String prenom_personne) {
-        this.prenom_personne = prenom_personne;
-    }
-
-    public String getNom_personne() {
-        return nom_personne;
-    }
-
-    public void setNom_personne(String nom_personne) {
-        this.nom_personne = nom_personne;
-    }
-
-    public String getNature_juridique() {
-        return nature_juridique;
-    }
-
-    public void setNature_juridique(String nature_juridique) {
-        this.nature_juridique = nature_juridique;
-    }
-
-    public String getLocalisation() {
-        return localisation;
-    }
-
-    public void setLocalisation(String localisation) {
-        this.localisation = localisation;
-    }
-*/
 }
